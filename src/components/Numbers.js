b50682894daf4970f45c78c43b66b7fc0b5bba82
lohi212 <suvvarilohita@gmail.com>
2023-03-08 18:22:09 +0530

    feat: initial render

---
+++ src/components/Numbers.js
@@ -0,0 +1,12 @@
+import React from "react";
+import { Text } from "react-native";
+
+const Numbers = ({ finalStr }) => {
+  return (
+    <View>
+      <Text>{finalStr}</Text>
+    </View>
+  );
+};
+
+export default Numbers;
