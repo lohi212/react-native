934161b549a48b24377d59827facc8988f7c3e26
lohi212 <suvvarilohita@gmail.com>
2023-03-08 18:23:13 +0530

    feat(preview): preview screen

---
+++ src/components/Preview.js
@@ -0,0 +1,12 @@
+import React from "react";
+import { Text } from "react-native";
+
+const Preview = ({ finalStr }) => {
+  return (
+    <View>
+      <Text>{finalStr}</Text>
+    </View>
+  );
+};
+
+export default Preview;
